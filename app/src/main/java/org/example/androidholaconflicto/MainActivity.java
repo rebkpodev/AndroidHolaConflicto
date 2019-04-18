91cd77b13292393bb3056be14f7e5f5ea6af8905
Programador2 <programador2@ejemplo.org>
2019-04-18 23:04:55 +0200

    Programador 2 pone un comentario en MainActivity.java

--- app/src/main/java/org/example/androidholaconflicto/MainActivity.java
+++ app/src/main/java/org/example/androidholaconflicto/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    //comentario añadido por Programador 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
